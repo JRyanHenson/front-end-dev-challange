53228937e2193a80339d89450306444ea3c12819
JRyanHenson <hensojr@gmail.com>
2017-08-07 12:06:31 -0600

    Updated bootstrap content

--- index.php
+++ index.php
@@ -27,63 +27,86 @@
 		<link rel="stylesheet" href="css/styles.css" type="text/css">
 	</head>
 	<body class="sfooter">
-
 		<div class="sfooter-content">
 			<!-- your page content here-->
-		<div class="container">
-			<nav class="navbar navbar-default">
-				<!-- Title and toggle get grouped for better mobile display -->
-				<div class="navbar-header">
-					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="#">Title</a>
+			<div class="container">
+				<nav class="navbar navbar-default">
+					<!-- Title and toggle get grouped for better mobile display -->
+					<div class="navbar-header">
+						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+							<span class="sr-only">Toggle navigation</span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+							<span class="icon-bar"></span>
+						</button>
+						<a class="navbar-brand" href="#">Title</a>
+					</div>
+					<!-- Collect the nav links for toggling -->
+					<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+						<ul class="nav navbar-nav navbar-right">
+							<li><a href="#">Link 1</a></li>
+							<li><a href="#">Link 2</a></li>
+							<li><a href="#">Link 3</a></li>
+							<li><a href="#">Link 4</a></li>
+						</ul>
+					</div><!-- /.navbar-collapse -->
+				</nav>
+				<div class="jumbotron">
+					<h1>Hello, world!</h1>
+					<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+					<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
 				</div>
-				<!-- Collect the nav links for toggling -->
-				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-					<ul class="nav navbar-nav navbar-right">
-						<li><a href="#">Link 1</a></li>
-						<li><a href="#">Link 2</a></li>
-						<li><a href="#">Link 3</a></li>
-						<li><a href="#">Link 4</a></li>
-					</ul>
-				</div><!-- /.navbar-collapse -->
-			</nav>
-			<div class="jumbotron">
-				<h1>Hello, world!</h1>
-				<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
-				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
-			</div>
-		</div><!-- /.container -->
-		<div class="container"
-			<div class="row">
-				<div class="col-md-2" >
-					<img src="https://www.fillmurray.com/200/300" class="img-responsive" alt="Responsive image">
+			</div><!-- /.container -->
+			<div class="container-fluid">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-2" >
+							<img src="https://www.fillmurray.com/200/300" class="img-responsive" alt="Responsive image">
+						</div>
+						<div class="col-md-10" >
+							<p>Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
+								Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
+								brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami. Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
+								Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
+								brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami.</p>
+							<p>Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
+								Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
+								brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami.Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
+								Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
+								brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami.</p>
+						</div>
+					</div>
 				</div>
-				<div class="col-md-10" >
-					<p>Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
-						Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
-						brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami. Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
-						Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
-						brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami.</p>
-					<p>Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
-						Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
-						brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami.Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
-						Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
-						brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami.</p>
+			</div><!-- /.container-fluid -->
+			<div class="container"><!-- /.container -->
+				<div class="row">
+					<div class="col-md-4">
+						<img src="https://www.fillmurray.com/200/300" class="img-circle" alt="Responsive image">
+					</div>
+					<div class="col-md-4">
+						<img src="https://www.fillmurray.com/200/300" class="img-circle" alt="Responsive image">
+					</div>
+					<div class="col-md-4">
+						<img src="https://www.fillmurray.com/200/300" class="img-circle" alt="Responsive image">
+					</div>
 				</div>
-			</div>
+				<div class="row">
+					<div class="col-md-4">
+						<p>text</p>
+					</div>
+					<div class="col-md-4">
+						<p>text</p>
+					</div>
+					<div class="col-md-4">
+						<p>text</p>
+					</div>
+				</div>
+			</div><!-- /.container -->
+			<footer>
+				<!-- footer here-->
+				<p>Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage</p>
+			</footer>
 		</div>
-		</div>
-		<footer>
-			<!-- footer here-->
-			<div class="container"
-			<p>Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage</p>
-			</div>
-		</footer>>
 	</body>
 
 </html>
