51e222df278abcaafa6b498dd5b90147ab4c750b
JRyanHenson <hensojr@gmail.com>
2017-08-06 20:40:33 -0600

    Added bootstrap content

--- index.php
+++ index.php
@@ -1,31 +1,89 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-	<meta charset="utf-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta charset="utf-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
 
-	<!-- Bootrap Latest cmmpliled and minified CSS -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
+		<!-- Bootrap Latest cmmpliled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous" />
 
-	<!-- Optional Theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
+		<!-- Optional Theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous"/>
 
-	<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!-- [if lt IE 9]>
-	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif] -->
+		<!-- HTML5 shiv and Respond.js for IE8 support of HTML5 elements and media queries -->
+		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+		<!-- [if lt IE 9]>
+		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+		<![endif] -->
 
-	<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
+		<!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+		<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.0.0/jquery.min.js"></script>
 
-	<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+		<!-- Latest compiled and minified Bootstrap JavaScript, all compiled plugins included -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+		<link rel="stylesheet" href="css/styles.css" type="text/css">
 	</head>
-	<body>
+	<body class="sfooter">
 
+		<div class="sfooter-content">
+			<!-- your page content here-->
+		<div class="container">
+			<nav class="navbar navbar-default">
+				<!-- Title and toggle get grouped for better mobile display -->
+				<div class="navbar-header">
+					<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+						<span class="sr-only">Toggle navigation</span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+						<span class="icon-bar"></span>
+					</button>
+					<a class="navbar-brand" href="#">Title</a>
+				</div>
+				<!-- Collect the nav links for toggling -->
+				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#">Link 1</a></li>
+						<li><a href="#">Link 2</a></li>
+						<li><a href="#">Link 3</a></li>
+						<li><a href="#">Link 4</a></li>
+					</ul>
+				</div><!-- /.navbar-collapse -->
+			</nav>
+			<div class="jumbotron">
+				<h1>Hello, world!</h1>
+				<p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+				<p><a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+			</div>
+		</div><!-- /.container -->
+		<div class="container"
+			<div class="row">
+				<div class="col-md-2" >
+					<img src="https://www.fillmurray.com/200/300" class="img-responsive" alt="Responsive image">
+				</div>
+				<div class="col-md-10" >
+					<p>Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
+						Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
+						brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami. Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
+						Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
+						brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami.</p>
+					<p>Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
+						Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
+						brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami.Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage, shoulder tenderloin venison landjaeger jowl.
+						Chicken flank pancetta, ham jerky pork chop chuck. Doner turkey shoulder chicken corned beef. Turducken boudin
+						brisket picanha, meatball corned beef alcatra rump cow shoulder pastrami.</p>
+				</div>
+			</div>
+		</div>
+		</div>
+		<footer>
+			<!-- footer here-->
+			<div class="container"
+			<p>Bacon ipsum dolor amet pork turkey chuck tri-tip salami sausage</p>
+			</div>
+		</footer>>
 	</body>
 
 </html>
